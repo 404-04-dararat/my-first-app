058daa790d3fe88e4767b42236ca3870c4ad63a0
404-04-dararat <54254@yupparaj.ac.th>
2026-08-05 16:12:03 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -3,7 +3,7 @@ st.title("🛒แอปพลิเคชั่นคำนวณราคาส
 price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
 vat = price * 0.07
 net_price = price - vat
-st.divider()
 st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
 st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
+st.divider()
 st.write("นางสาวดารารัตน์ นันทกุลพลพินิจ เลขที่ 4 ม.4/4")
