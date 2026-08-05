f0a2b1a178f7664ee84c850641d91d0828742d2f
404-04-dararat <54254@yupparaj.ac.th>
2026-08-05 16:05:01 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 st.title("🛒แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 7%")
 price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
-st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
-net_price = price - vat
 vat = price * 0.07
-st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
+net_price = price - vat
 st.divider()
+st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
+st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
 st.write("นางสาวดารารัตน์ นันทกุลพลพินิจ เลขที่ 4 ม.4/4")
